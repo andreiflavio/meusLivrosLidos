de7779e593829af760ed80243ad8b9e43f0ce4b0
Flavio Andrei <andrei.flavio@hotmail.com>
2019-03-17 19:43:44 -0300

    Implementada melhoria no endpoint GET da API para permitir pesquisa e ordenação decrescente por data de cadastro do livro

--- meusLivrosLidos/api/views.py
+++ meusLivrosLidos/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.db.models import Q
 from rest_framework.viewsets import ModelViewSet
 from .serializers import LivroSerializer, UserSerializer
 
@@ -10,7 +11,12 @@ class LivroAPIViewSet(ModelViewSet):
     serializer_class = LivroSerializer
 
     def get_queryset(self):    	
-    	return Livro.objects.filter(user=self.request.user).order_by('-data_cadastro')
+    	queryset = Livro.objects.filter(user=self.request.user).order_by('-data_cadastro')
+    	search = self.request.query_params.get('search', None)
+    	if search is not None:
+    		return queryset.filter(Q(nome__icontains=search) | Q(autor__icontains=search))
+    	else:
+    		return queryset
     
 
 class UsuarioAPIViewSet(ModelViewSet):
